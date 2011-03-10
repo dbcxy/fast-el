486b8fe150a4d41d72399fc2a3beedc78e2038af
greenpineyu <greenpineyu@2112a917-312c-4bfb-8744-553e886d0a7d>
2011-03-10 07:10:20 +0000

    重构代码

--- trunk/src/com/greenpineyu/fel/interpreter/ConstInterpreter.java
+++ trunk/src/com/greenpineyu/fel/interpreter/ConstInterpreter.java
@@ -1,6 +1,7 @@
-package com.greenpineyu.fel.parser;
+package com.greenpineyu.fel.interpreter;
 
 import com.greenpineyu.fel.context.FelContext;
+import com.greenpineyu.fel.parser.FelNode;
 
 public class ConstInterpreter implements Interpreter {
 
--- trunk/src/com/greenpineyu/fel/interpreter/Interpreter.java
+++ trunk/src/com/greenpineyu/fel/interpreter/Interpreter.java
@@ -1,6 +1,7 @@
-package com.greenpineyu.fel.parser;
+package com.greenpineyu.fel.interpreter;
 
 import com.greenpineyu.fel.context.FelContext;
+import com.greenpineyu.fel.parser.FelNode;
 
 /**
  * 解析器,用于解析AstNode的值
