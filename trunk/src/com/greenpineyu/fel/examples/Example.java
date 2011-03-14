76b78471f3b13ada2903d336b04051068e82b524
greenpineyu <greenpineyu@2112a917-312c-4bfb-8744-553e886d0a7d>
2011-03-14 08:59:25 +0000

    加入自定义解释器的例子

--- trunk/src/com/greenpineyu/fel/examples/Example.java
+++ trunk/src/com/greenpineyu/fel/examples/Example.java
@@ -1,10 +1,16 @@
 package com.greenpineyu.fel.examples;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang.mutable.MutableInt;
+
 import com.greenpineyu.fel.FelEngine;
 import com.greenpineyu.fel.FelEngineImpl;
 import com.greenpineyu.fel.context.AbstractContext;
 import com.greenpineyu.fel.context.FelContext;
 import com.greenpineyu.fel.interpreter.ConstInterpreter;
+import com.greenpineyu.fel.interpreter.Interpreter;
 import com.greenpineyu.fel.parser.FelNode;
 
 public class Example {
@@ -69,8 +75,68 @@ public class Example {
 		node.eval(rootContext);
 	}
 
+	/**
+	 * 自定义解释器的高级用法
+	 */
+	public static void userInterpreterX() {
+		/*
+		 * 假设数据库中有两列，单价和数量。
+		 * 现在需要通过表达式计算金额(表达式:单价*数量)。
+		 * 通常的做法是通过context获取变量（单价、数量的值），
+		 * 在小数量里时，这种做法是很好的，但是大数量量时，性能就很差了。
+		 * 如果使用自定义的解释器，会提高效率。
+		 */
+		
+
+		//数据库中单价列的记录
+		String[] price = new String[] {
+				"2", "3", "4"
+		};
+		//数据库中数量列的记录
+		String[] number = new String[] {
+				"10.99", "20.99", "9.9"
+		};
+
+		String exp = "单价*数量";
+		FelNode node = fel.parse(exp);
+		List<FelNode> children = node.getChildren();
+		MutableInt index = new MutableInt(0);
+		//替换节点的解释器
+		for (Iterator iterator = children.iterator(); iterator.hasNext();) {
+			FelNode child = (FelNode) iterator.next();
+			if ("单价".equals(child.getText())) {
+				child.setInterpreter(new ColumnInterpreter(index, price));
+			} else if ("数量".equals(child.getText())) {
+				child.setInterpreter(new ColumnInterpreter(index, number));
+			}
+		}
+
+		for (int i = 0; i < number.length; i++) {
+			index.setValue(i);
+			Object eval = node.eval(null);
+			System.out.println("金额[" + price[i] + "*" + number[i] + "=" + eval + "]");
+		}
+	}
+
+
 	public static void main(String[] args) {
-		contexts();
+		userInterpreterX();
+	}
+
+}
+
+class ColumnInterpreter implements Interpreter {
+	MutableInt index;
+
+	String[] records;
+
+	ColumnInterpreter(MutableInt index, String[] records) {
+		this.index = index;
+		this.records = records;
+	}
+
+	public Object interpret(FelContext context, FelNode node) {
+		return records[index.intValue()];
 	}
 
 }
