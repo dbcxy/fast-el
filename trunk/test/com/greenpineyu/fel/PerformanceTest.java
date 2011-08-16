aeb966b6115bdc79f231e147f40668e067853966
greenpineyu <greenpineyu@2112a917-312c-4bfb-8744-553e886d0a7d>
2011-08-16 14:12:26 +0000

    上传例子和性能测试

--- trunk/test/com/greenpineyu/fel/PerformanceTest.java
+++ trunk/test/com/greenpineyu/fel/PerformanceTest.java
@@ -1,10 +1,45 @@
 package com.greenpineyu.fel;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 import com.greenpineyu.fel.context.FelContext;
 
 public class PerformanceTest {
 
 	public static void main(String[] args) {
+		speed();
+		stable();
+	}
+	
+	public static void stable(){
+		final FelEngine engine = new FelEngineImpl();
+		final long start = System.currentTimeMillis();
+		final int times = 1;
+		Thread cur = Thread.currentThread();
+		for (int i = 0; i < times; i++) {
+			final int temp = i;
+			Thread t = new Thread(){
+				public void run() {
+					for (int j = 0; j < times; j++) {
+						String str = temp+"+"+j;
+						long s = System.currentTimeMillis();
+						Expression exp = engine.compiler(str, null);
+						long now = System.currentTimeMillis();
+						Object value = exp.eval(null);
+						System.out.println(now-start+":"+(now-s)+"****************:"+str+"="+value);
+					}
+				};
+			};
+			t.start();
+//			try {
+////				t.join();
+//			} catch (InterruptedException e) {
+//			}
+		}
+		System.out.println("cost:"+(System.currentTimeMillis()-start));
+	}
+
+	private static void speed() {
 		FelEngine engine = new FelEngineImpl();
 		FelContext ctx = engine.getContext();
 		ctx.set("i", 100);
@@ -25,23 +60,25 @@ public class PerformanceTest {
 		System.out.println((1 - d * pi) - (2 + 100 - i / pi) % 99  );
 		System.out.println("value:"+engine.eval(s));
 		System.out.println("value:"+engine.compiler(s, null).eval(ctx));
-		 str1 = "i * pi + (d * b - 199) / (1 - d * pi) - (2 + 100 - i / pi) % 99 ==i * pi + (d * b - 199) / (1 - d * pi) - (2 + 100 - i / pi) % 99 ";
+//		 str1 = "i * pi + (d * b - 199) / (1 - d * pi) - (2 + 100 - i / pi) % 99 ==i * pi + (d * b - 199) / (1 - d * pi) - (2 + 100 - i / pi) % 99 ";
 		int times = 1000 * 1000;
-		//eval(engine, ctx, str1, times);
+		eval(engine, ctx, str1, times);
 	}
 
 	private static void eval(FelEngine engine, FelContext ctx, String str1,
 			int times) {
-		Expression compiler = engine.compiler(str1, null);
 		long start = System.currentTimeMillis();
+		Expression compiler = engine.compiler(str1, null);
 		Object result = null;
 		for (int i = 0; i < times; i++) {
 			result = compiler.eval(ctx);
 //			result = engine.eval(str1);
 		}
 		long end = System.currentTimeMillis();
-		System.out.println(end-start);
+		System.out.println("cost:"+(end-start));
 		 System.out.println(result);
 	}
+	
+	
 
 }
