d92fce391586c541112a638fb9441d3cd08d33da
greenpineyu <greenpineyu@2112a917-312c-4bfb-8744-553e886d0a7d>
2011-08-27 08:58:56 +0000

    重写例子

--- src/com/greenpineyu/fel/compile/Template.java
+++
@@ -1,8 +0,0 @@
-//package com.greenpineyu.fel.compile;
-//import com.greenpineyu.fel.Expression;
-//import com.greenpineyu.fel.context.FelContext;
-//public class ${classname} implements Expression{
-//	public Object eval(FelContext context) {
-//		return ${expression};
-//	}
-//}
