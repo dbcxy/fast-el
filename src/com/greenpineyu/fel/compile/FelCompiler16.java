f9630579160e89c884bf84d948eafb13ba1e059b
greenpineyu <greenpineyu@2112a917-312c-4bfb-8744-553e886d0a7d>
2011-09-12 01:01:36 +0000

    去年注释代码

--- src/com/greenpineyu/fel/compile/FelCompiler16.java
+++ src/com/greenpineyu/fel/compile/FelCompiler16.java
@@ -24,7 +24,6 @@ import javax.tools.ToolProvider;
 import com.greenpineyu.fel.Expression;
 
 public class FelCompiler16<T> implements FelCompiler {
-	static final String JAVA_EXTENSION = ".java";
 	private final FelCompilerClassloader classLoader;
 
 	private final JavaCompiler compiler;
@@ -67,32 +66,22 @@ public class FelCompiler16<T> implements FelCompiler {
 			}
 		}
 
-		// javaFileManager = new JavaFileManagerImpl(fileManager, classLoader);
 		javaFileManager = new ForwardingJavaFileManager<JavaFileManager>(
 				fileManager) {
 			 @Override
 			 public JavaFileObject getJavaFileForOutput(Location location,
 					String qualifiedName, Kind kind, FileObject outputFile)
 			 throws IOException {
-				// JavaFileObject file = new FelJavaFileObject(qualifiedName,
-				// kind);
-				// System.out.println(outputFile);
 				// 由于编译成功后的bytecode需要放到file中，所以需要将file放到classloader中，以便读取bytecode生成Class对象.
 				classLoader.add(qualifiedName, outputFile);
 				return (JavaFileObject) outputFile;
 			 }
 		};
 		this.options = new ArrayList<String>();
-		// if (options != null) { // make a save copy of input options
-		// for (String option : options) {
-		// this.options.add(option);
-		// }
-		// }
 	}
 
 	public Expression compile(JavaSource src) {
 
-		// Class<T> compile = compile(src.getName(), src.getSource(), null);
 		Class<T> compile = compileToClass(src);
 		try {
 			return (Expression) compile.newInstance();
@@ -104,24 +93,6 @@ public class FelCompiler16<T> implements FelCompiler {
 		return null;
 	}
 
-	/*public synchronized Class<T> compile(final String className,
-			final CharSequence javaSource,
-			final DiagnosticCollector<JavaFileObject> diagnosticsList)
-			throws ClassCastException {
-		if (diagnosticsList != null) {
-			diagnostics = diagnosticsList;
-		} else {
-			diagnostics = new DiagnosticCollector<JavaFileObject>();
-		}
-
-		Map<String, CharSequence> classes = new HashMap<String, CharSequence>(1);
-		classes.put(className, javaSource);
-
-		Map<String, Class<T>> compiled = compile(classes, diagnosticsList);
-		Class<T> newClass = compiled.get(className);
-
-		return newClass;
-	}*/
 
 	public synchronized Class<T> compileToClass(final JavaSource src) {
 		List<JavaFileObject> compileSrcs = new ArrayList<JavaFileObject>();
@@ -129,8 +100,6 @@ public class FelCompiler16<T> implements FelCompiler {
 		final FelJavaFileObject compileSrc = new FelJavaFileObject(className,
 				src.getSource());
 		compileSrcs.add(compileSrc);
-		// javaFileManager.putFileForInput(StandardLocation.SOURCE_PATH,
-		// src.getPackageName(), className + JAVA_EXTENSION, compileSrc);
 		final CompilationTask task = compiler.getTask(null, javaFileManager,
 				diagnostics, options, null, compileSrcs);
 		final Boolean result = task.call();
@@ -147,65 +116,11 @@ public class FelCompiler16<T> implements FelCompiler {
 		return null;
 	}
 
-	/*public synchronized Map<String, Class<T>> compile(
-			final Map<String, CharSequence> classes,
-			final DiagnosticCollector<JavaFileObject> diagnosticsList) {
-		List<JavaFileObject> sources = new ArrayList<JavaFileObject>();
-		for (Entry<String, CharSequence> entry : classes.entrySet()) {
-			String qualifiedClassName = entry.getKey();
-			CharSequence javaSource = entry.getValue();
-			if (javaSource != null) {
-				final int dotPos = qualifiedClassName.lastIndexOf('.');
-				final String className = dotPos == -1 ? qualifiedClassName
-						: qualifiedClassName.substring(dotPos + 1);
-				final String packageName = dotPos == -1 ? ""
-						: qualifiedClassName.substring(0, dotPos);
-				final FelJavaFileObject source = new FelJavaFileObject(
-						className, javaSource);
-				sources.add(source);
-
-				// javaFileManager.putFileForInput(StandardLocation.SOURCE_PATH,
-				// packageName, className + JAVA_EXTENSION, source);
-			}
-		}
-
-		// Get a CompliationTask from the compiler and compile the sources
-		final CompilationTask task = compiler.getTask(null, javaFileManager,
-				diagnostics, options, null, sources);
-		final Boolean result = task.call();
-		if (result == null || !result.booleanValue()) {
-			// 编译失败
-			return null;
-		}
-
-		try {
-			// For each class name in the inpput map, get its compiled class and
-			// put it in the output map
-			Map<String, Class<T>> compiled = new HashMap<String, Class<T>>();
-			for (String qualifiedClassName : classes.keySet()) {
-				final Class<T> newClass = loadClass(qualifiedClassName);
-				compiled.put(qualifiedClassName, newClass);
-			}
-
-			return compiled;
-		} catch (ClassNotFoundException e) {
-			return null;
-		} catch (IllegalArgumentException e) {
-			return null;
-		} catch (SecurityException e) {
-			return null;
-		}
-	}*/
-
 	@SuppressWarnings("unchecked")
 	public Class<T> loadClass(final String qualifiedClassName)
 			throws ClassNotFoundException {
 		return (Class<T>) classLoader.loadClass(qualifiedClassName);
 	}
-
-	// public ClassLoader getClassLoader() {
-	// return javaFileManager.getClassLoader();
-	// }
 
 	static URI toUri(String name) {
 		try {
@@ -217,133 +132,3 @@ public class FelCompiler16<T> implements FelCompiler {
 
 }
 
-/**
- * 用于编译的文件管理器
- * 
- * @author yuqingsong
- * 
- */
-
-// final class JavaFileManagerImpl extends
-// ForwardingJavaFileManager<JavaFileManager> {
-// private final FelCompilerClassloader classloader;
-//
-// // private final Map<URI, JavaFileObject> compileSrc = new HashMap<URI,
-// // JavaFileObject>();
-//
-// public JavaFileManagerImpl(JavaFileManager fileManager,
-// FelCompilerClassloader classloader) {
-// super(fileManager);
-// this.classloader = classloader;
-// }
-//
-// public ClassLoader getClassLoader() {
-// return classloader;
-// }
-//
-// /* @Override
-// public FileObject getFileForInput(Location location, String packageName,
-// String relativeName) throws IOException {
-// // 源文件只使用一次
-// FileObject o = compileSrc.get(uri(location, packageName,
-// relativeName));
-// if (o != null) {
-// return o;
-// }
-// return super.getFileForInput(location, packageName, relativeName);
-// }*/
-//
-// /**
-// * 添加需要编译的文件
-// *
-// * @param location
-// * @param packageName
-// * @param relativeName
-// * @param file
-// */
-// /*public void putFileForInput(StandardLocation location, String packageName,
-// String relativeName, JavaFileObject file) {
-// compileSrc.put(uri(location, packageName, relativeName), file);
-// }*/
-//
-// private URI uri(Location location, String packageName, String relativeName) {
-// return Compiler16.toUri(location.getName() + '/' + packageName + '/'
-// + relativeName);
-// }
-//
-// /*
-// * 获取需要编译的源文件
-// * @see
-// javax.tools.ForwardingJavaFileManager#getJavaFileForOutput(javax.tools.JavaFileManager.Location,
-// java.lang.String, javax.tools.JavaFileObject.Kind, javax.tools.FileObject)
-// */
-// @Override
-// public JavaFileObject getJavaFileForOutput(Location location,
-// String qualifiedName, Kind kind, FileObject outputFile)
-// throws IOException {
-// // JavaFileObject file = new FelJavaFileObject(qualifiedName, kind);
-// // System.out.println(outputFile);
-// // 由于编译成功后的bytecode需要放到file中，所以需要将file放到classloader中，以便读取bytecode生成Class对象.
-// classloader.add(qualifiedName, outputFile);
-// return (JavaFileObject) outputFile;
-// }
-//
-// @Override
-// public ClassLoader getClassLoader(JavaFileManager.Location location) {
-// return classloader;
-// }
-//
-// /*
-// @Override
-// public String inferBinaryName(Location loc, JavaFileObject file) {
-// if (file instanceof FelJavaFileObject) {
-// return file.getName();
-// }
-//
-// return super.inferBinaryName(loc, file);
-// }
-// */
-//
-// /*@Override
-// public Iterable<JavaFileObject> list(Location location, String packageName,
-// Set<Kind> kinds, boolean recurse) throws IOException {
-// Iterable<JavaFileObject> result = super.list(location, packageName,
-// kinds, recurse);
-//
-// ClassLoader contextClassLoader = Thread.currentThread()
-// .getContextClassLoader();
-// List<URL> urlList = new ArrayList<URL>();
-// Enumeration<URL> e = contextClassLoader.getResources("com");
-// while (e.hasMoreElements()) {
-// urlList.add(e.nextElement());
-// }
-//
-// ArrayList<JavaFileObject> files = new ArrayList<JavaFileObject>();
-//
-// if (location == StandardLocation.CLASS_PATH
-// && kinds.contains(JavaFileObject.Kind.CLASS)) {
-// for (JavaFileObject file : fileObjects.values()) {
-// if (file.getKind() == Kind.CLASS
-// && file.getName().startsWith(packageName)) {
-// files.add(file);
-// }
-// }
-//
-// files.addAll(classLoader.files());
-// } else if (location == StandardLocation.SOURCE_PATH
-// && kinds.contains(JavaFileObject.Kind.SOURCE)) {
-// for (JavaFileObject file : fileObjects.values()) {
-// if (file.getKind() == Kind.SOURCE
-// && file.getName().startsWith(packageName)) {
-// files.add(file);
-// }
-// }
-// }
-//
-// for (JavaFileObject file : result) {
-// files.add(file);
-// }
-//
-// return files;
-// }*/
-// }
--- src/com/greenpineyu/fel/compile/FelCompilerClassloader.java
+++ src/com/greenpineyu/fel/compile/FelCompilerClassloader.java
@@ -12,14 +12,6 @@ public final class FelCompilerClassloader extends ClassLoader {
 		super(parentClassLoader);
 	}
 
-	//
-	// public Collection<FileObject> files() {
-	// return Collections.unmodifiableCollection(compileFileMap.values());
-	// }
-	//
-	// public void clearCache() {
-	// this.compileFileMap.clear();
-	// }
 
 	@Override
 	protected synchronized Class<?> findClass(String qualifiedClassName) throws ClassNotFoundException {
@@ -28,21 +20,6 @@ public final class FelCompilerClassloader extends ClassLoader {
 			byte[] bytes = ((FelJavaFileObject) file).getByteCode();
 			return defineClass(qualifiedClassName, bytes, 0, bytes.length);
 		}
-		
-
-		// try {
-		// return Class.forName(qualifiedClassName);
-		// } catch (ClassNotFoundException nf) {
-		// // Ignore and fall through
-		// }
-		//
-		// try {
-		// return
-		// Thread.currentThread().getContextClassLoader().loadClass(qualifiedClassName);
-		// } catch (ClassNotFoundException nf) {
-		// // Ignore and fall through
-		// }
-		
 		return super.findClass(qualifiedClassName);
 	}
 
@@ -50,15 +27,4 @@ public final class FelCompilerClassloader extends ClassLoader {
 		compileFileMap.put(qualifiedClassName, javaFile);
 	}
 
-	/*
-	 * @Override public InputStream getResourceAsStream(final String name) { if
-	 * (name.endsWith(".class")) { String qualifiedClassName = name.substring(0,
-	 * name.length() - ".class".length()).replace('/', '.'); FelJavaFileObject
-	 * file = (FelJavaFileObject) compileFileMap .get(qualifiedClassName);
-	 * 
-	 * if (file != null) { return new ByteArrayInputStream(file.getByteCode());
-	 * } }
-	 * 
-	 * return super.getResourceAsStream(name); }
-	 */
 }
